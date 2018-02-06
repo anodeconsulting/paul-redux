5bbf38d1f29182b59a5f2280e8f6e45e8918bfee
jeremyxuzhe <xz90820@hotmail.com>
2018-02-06 16:49:48 -0500

    save

---
+++ src/components/rightbox/rightbox.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import './rightbox.css';
+
+class RightBox extends Component {
+    render() {
+        return (
+            <div>
+                <div className="right-box">
+                    <i className="fa fa-caret-right right-box-arrow" aria-hidden="true"></i>
+                    <h4 className="right-box-content">Recomendaciones de Seguridad</h4>
+                </div>
+                <div className="right-box">
+                    <i className="fa fa-caret-right right-box-arrow" aria-hidden="true"></i>
+                    <h4 className="right-box-content">¿Necesitas Ayuda?</h4>
+                </div>
+                <div className="right-box">
+                    <i className="fa fa-caret-right right-box-arrow" aria-hidden="true"></i>
+                    <h4 className="right-box-content">Sucursales y Cajeros Automáticos</h4>
+                </div>
+                <div className="right-box">
+                    <i className="fa fa-caret-right right-box-arrow" aria-hidden="true"></i>
+                    <h4 className="right-box-content">Contáctanos</h4>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+
+export default RightBox ;
