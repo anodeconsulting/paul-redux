a87e54a6b4e5aa5f51733e1ae7135d9ca57503a3
jeremyxuzhe <xz90820@hotmail.com>
2018-03-07 11:46:37 -0500

    save

--- src/components/Tools/RedDropdownBtn/RedDropDownBtn.js
+++ src/components/Tools/RedDropdownBtn/RedDropDownBtn.js
@@ -4,17 +4,17 @@ class RedDropDownBtn extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            open: false
-        };
     }
+
+
+
     render(){
 
         return(
             <span >
 
-              <i className={!this.state.open?"fa fa-square square-btn red":"fa fa-square square-btn white square-border"} aria-hidden="true"></i>
-              <i className={!this.state.open?"fa fa-sort-desc arrow-down-btn white":"fa fa-sort-desc arrow-down-btn black"} aria-hidden="true"></i>
+              <i className={!this.props.open?"fa fa-square square-btn red":"fa fa-square square-btn white square-border"} aria-hidden="true"></i>
+              <i className={!this.props.open?"fa fa-sort-desc arrow-down-btn white":"fa fa-sort-desc arrow-down-btn black"} aria-hidden="true"></i>
 
             </span>
         )
