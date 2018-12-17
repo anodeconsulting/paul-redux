6578c83e0e5ebbf8184a5a26f2c42ccc9cf01abf
paul wu <pwu@nextpathway.com>
2018-12-17 09:30:07 -0500

    update host name

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
   // mongoURI: 'mongodb://servicefactory:P%4055w0rd@ds155292.mlab.com:55292/mern_sf'
-  mongoURI: "mongodb://mongo:27017/billingdb",
+  mongoURI: "mongodb://mgoentrypoint:27017/billingdb",
   mongoUser: "k8sblademan1@np.com",
   mongoPass: "test"
 };
