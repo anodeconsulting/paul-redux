3a42b3c1bb9a9e0b8359aab58026be4ce25259ab
paul wu <pwu@nextpathway.com>
2018-12-18 12:03:59 -0500

    added host

---
+++ client/src/config/config.js
@@ -0,0 +1,3 @@
+export const config = {
+  proxy: "http://app:5000"
+};
--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
 module.exports = {
   // mongoURI: 'mongodb://servicefactory:P%4055w0rd@ds155292.mlab.com:55292/mern_sf'
-  mongoURI: "mongodb://root:XmFcyhz3nnwbzNvu@mgoentrypoint:27017/billingdb",
+  // mongoURI: "mongodb://root:XmFcyhz3nnwbzNvu@mgoentrypoint:27017/billingdb",
+  mongoURI: "mongodb://mongo:27017/billingdb",
   mongoUser: "k8sblademan1@np.com",
   mongoPass: "test"
 };
