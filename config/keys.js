d5560d106350db6cce7d7225c0cedc60d48ea70e
jeremyxuzhe <xz90820@hotmail.com>
2018-12-19 16:42:54 -0500

    save

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,8 @@ module.exports = {
   // mongoURI: 'mongodb://servicefactory:P%4055w0rd@ds155292.mlab.com:55292/mern_sf'
   // mongoURI: "mongodb://root:XmFcyhz3nnwbzNvu@mgoentrypoint:27017/billingdb",
   mongoURI:
-    "mongodb://root:XmFcyhz3nnwbzNvu@mgoentrypoint.dbdemo.svc.cluster.local:27017/kubedb?authSource=admin",
+    // "mongodb://root:XmFcyhz3nnwbzNvu@mgoentrypoint.dbdemo.svc.cluster.local:27017/kubedb?authSource=admin",
+  "mongodb://mongo:27017/billingUser",
   mongoUser: "k8sblademan1@np.com",
   mongoPass: "test"
 };
